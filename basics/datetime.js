2c5df3ad211367581c624b696fd76577fc5cbf92
skyp <skypolist11@gmail.com>
2025-02-10 17:01:10 +0000

    date time

---
+++ basics/datetime.js
@@ -0,0 +1,11 @@
+let mydate =new Date()
+// console.log(mydate.toString())
+// console.log(mydate.toDateString())
+// console.log(mydate.toISOString())
+// console.log(mydate.toJSON())
+//console.log(mydate.toLocaleString())
+// date is an object in javascript
+//to create a date
+// let mycreateddate = new Date(2025,1,10)
+// console.log(mycreateddate.toDateString())
+console.log(mydate.getDay());
